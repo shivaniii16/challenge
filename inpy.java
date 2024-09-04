3b1bb63fc072dd2d893f3e4b3a83be74b6ee3d2f
Shivani Gore <70757166+shivaniii16@users.noreply.github.com>
2024-09-04 17:39:54 +0530

    Add files via upload

---
+++ inpy.java
@@ -0,0 +1,22 @@
+public class inpy {
+    public static void main(String[] args) {
+        int n = 5; 
+
+     
+ for (int i = n; i >= 1; i--) {
+ for (int j = 0; j < n - i; j++) {
+       System.out.print(" ");
+     }
+            
+  for (int j = 1; j <= 2 * i - 1; j++) {
+     if (j == 1 || j == 2 * i - 1 || i==n) {
+         System.out.print("*");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+           
+            System.out.println();
+        }
+    }
+}
